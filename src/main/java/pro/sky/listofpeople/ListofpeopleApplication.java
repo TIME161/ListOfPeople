1a7ffb9177cc99e68448a6c8eb49b27448c5c4e0
TIME <opout777@yandex.ru>
2023-03-02 16:08:16 +0300

    First commit

---
+++ src/main/java/pro/sky/listofpeople/ListofpeopleApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.listofpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListofpeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListofpeopleApplication.class, args);
+	}
+
+}
